b4f5756057a9b896175bc929058733a682024638
Jyoti Mate <jyotismate20@gmail.com>
2021-10-25 12:04:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var navLinks = document.getElementById("navLinks");
+
+    function showMenu() {
+        navLinks.style.right = "0";
+    }
+    function hideMenu() {
+        navLinks.style.right = "-200px";
+    }
+
+var typed = new Typed('.type', {
+    strings: ['Front End Developer', 'WordPress Developer', 'Graphic Designer'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+   });
+
+   
